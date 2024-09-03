19f79961f1da6b66a7cbb50558bbc89bea28ae98
Tony Huang <tonyhchii@gmail.com>
2024-09-03 17:50:02 -0400

    added basic menu card functionality

--- src/contactPage.js
+++ src/contactPage.js
@@ -7,6 +7,17 @@ export function loadContact() {
 
     const title = document.createElement('h1');
     title.textContent = "Contact Us";
-
     container.appendChild(title);
+
+    const callBox = document.createElement('div');
+    callBox.classList.add('box');
+    container.appendChild(callBox);
+
+    const phoneNumber = document.createElement('p');
+    phoneNumber.textContent = "Call: 911-900-9000";
+    callBox.appendChild(phoneNumber);
+    
+    const email = document.createElement('p');
+    email.textContent = "Email: HeyItsTony@yahoo.com";
+    callBox.appendChild(email);
 }
--- src/menuPage.js
+++ src/menuPage.js
@@ -7,6 +7,30 @@ export function loadMenu() {
 
     const title = document.createElement('h1');
     title.textContent = "Menu";
-
     container.appendChild(title);
+
+    const menuBox = document.createElement('div');
+    menuBox.classList.add('grid');
+    container.appendChild(menuBox);
+
+    for(let i = 0; i < 9; i++){
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const leftCol = document.createElement('div');
+        const cardTitle = document.createElement('h2');
+        cardTitle.textContent = "Food Item" + i;
+        leftCol.appendChild(cardTitle);
+
+        const cardDes = document.createElement('p');
+        cardDes.textContent = "Food description";
+        leftCol.appendChild(cardDes);
+        card.appendChild(leftCol);
+
+        const cardImg = document.createElement('img');
+        cardImg.classList.add("card-img");
+        card.appendChild(cardImg);
+
+        menuBox.appendChild(card);
+    }
 }
